82f9786789164c20baddb751192df9bfc8c33b31
ryzone1 <101263465+ryzone1@users.noreply.github.com>
2026-07-08 18:01:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/secondEterPP/',
-  build: {
-    outDir: 'docs'
-  },
-  plugins: [react()]
+  plugins: [react()],
+  base: 'secondEterPP'
 })
